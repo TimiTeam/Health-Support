285b3e13dfae6cb19f20019e67380a9df60d5fde
Timur BUJALO <tbujalo@e1r9p10.unit.ua>
2020-01-15 18:23:10 +0200

    removing log4j properties

--- src/main/java/ua/unit/tbujalo/controllers/HomeController.java
+++ src/main/java/ua/unit/tbujalo/controllers/HomeController.java
@@ -9,15 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    private final Logger logger = Logger.getLogger(HomeController.class.getName());
-
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String homePage(Model model){
         model.addAttribute("message", "Hello dear guest!");
-        if (model.containsAttribute("welcome")){
-            logger.info("attribute is set");
-        }else
-            logger.error("attribute didn't set");
         return "home";
     }
 }
